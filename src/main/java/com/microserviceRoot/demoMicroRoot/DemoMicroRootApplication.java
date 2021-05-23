dbb566fb0a8d334e23106cce2c449287737c75e0
hemanthpatel1999 <hemanthpatel1999@gmail.com>
2021-05-23 21:09:47 +0530

    Initial commit

---
+++ src/main/java/com/microserviceRoot/demoMicroRoot/DemoMicroRootApplication.java
@@ -0,0 +1,13 @@
+package com.microserviceRoot.demoMicroRoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroRootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceRoot/demoMicroRoot/DemoMicroRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceRoot.demoMicroRoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
